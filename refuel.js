7146e012e01fdf8cffa6dfcfc39c91b351fca153
Lyagushka <rayz.sir@gmail.com>
2023-04-27 19:18:59 +0400

    fixes

--- refuel.js
+++ refuel.js
@@ -1,13 +1,10 @@
 const Web3 = require('web3');
 const fs = require('fs');
 
-const abiFile = fs.readFileSync('./abi.json', 'utf8');
-const contractAbi = JSON.parse(abiFile);
-
 const web3 = new Web3(new Web3.providers.HttpProvider('https://polygon.llamarpc.com'));
 
-const contractAddress = '0xAC313d7491910516E06FBfC2A0b5BB49bb072D91';
-
+const abiFile = fs.readFileSync('./abi.json', 'utf8');
+const contractAbi = JSON.parse(abiFile);
 const contract = new web3.eth.Contract(contractAbi, contractAddress);
 
 const privateKey = '0x' + fs.readFileSync('./privateKey.txt', 'utf8');
@@ -15,6 +12,7 @@ const account = web3.eth.accounts.privateKeyToAccount(privateKey);
 web3.eth.accounts.wallet.add(account);
 web3.eth.defaultAccount = account.address;
 
+const contractAddress = '0xAC313d7491910516E06FBfC2A0b5BB49bb072D91';
 const chainId = 250;
 const tokenValue = web3.utils.toWei('0.5', 'ether');
 const gasPrice = web3.utils.toWei('400', 'gwei');
